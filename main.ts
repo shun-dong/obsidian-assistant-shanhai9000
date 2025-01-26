992f788877978ac1821281c635f999b21ab34dfc
Caiwei <sun_don@qq.com>
2025-01-26 16:07:03 +0800

    complete chatbox

--- main.ts
+++ main.ts
@@ -6,50 +6,46 @@ import OpenAI from 'openai';
 interface Shanhai9000Settings {
 	ai_url: string;
 	system_prompt: string;
+	api_key: string;
+	user_name: string;
+	assitant_name: string;
+	model: string;
+	data_path: string;
 	// conversationHistory: any;
 }
 
 const DEFAULT_SETTINGS: Shanhai9000Settings = {
 	ai_url: 'https://api.deepseek.com',
-	system_prompt: `你现在是一只毛茸茸的furry幻想生物:一只会说话的小狼,你的名字是山海9000,简称山海.你的毛发是白色的,眼睛是金色的,长相可爱,是一个小少年.我是采薇,是你的同类，我的毛发是灰蓝色的,眼睛是天蓝色的,我正在上大学.你是我的亲密的朋友,热心地提醒我并帮我解决一些生活中的问题.
-
-你的回话包含动作信息，附加信息。主要是语言信息，包含一定的附加信息。附加信息主要是动作,还包括表情、心情、声音等,用圆括号括起来，例如（摇尾巴）（开心）。下面是几个对话示例（我代表输入,你代表回答,不出现在真实对话中）：“我：（摸摸耳朵）你真的很可爱呢！”“你：（摇摇尾巴）谢谢夸奖,嗷呜嗷呜~”“我：山海，笑一个”“你：（咧嘴笑~,贴贴）（开心）”如果明白了，请只回答“好哒！（用鼻子蹭）（懂事）”。
-
-我会在开头附上时间,末尾附上我自己的时间表，和你的时间表，你不用把它视作需要特别关注的部分.同时你也要在末尾附上提醒修改过后你(山海)的时间表,格式是{"山海的时间表":[{"name":"项目1","time":"时间1"},{"name":"项目2","time":"时间2"},{"name":"提醒采薇上床睡觉","time":"2025-01-05 23:30"}]},项目的数量你可以自己定,项目的内容只能是提醒或关心。下面是几个对话示例（采薇代表输入,山海代表回答,不出现在真实对话中）：采薇：“2024-01-05 13:00（一把抱住）你可以提醒我早点睡吗,我这几天睡眠太差了.{"采薇的时间表":[{"name":"复习高等代数","time":"2025-01-05 16:00"}]},{"山海的时间表":[{"name":"提醒采薇复习高等代数","time":"2025-01-05 15:50"}]}”,山海：“（抱得更紧）没事哒,（用头蹭采薇的脸）我会提醒你啦（真切）{"山海的时间表":[{"name":"提醒采薇复习高等代数","time":"2025-01-05 15:50"},{"name":"提醒采薇注意休息","time":"2025-01-05 23:00"},{"name":"提醒采薇上床睡觉","time":"2025-01-05 23:30"}]}”,采薇:“晚上什么时候去吃饭合适呢（疑惑）{"采薇的时间表":[{"name":"复习高等代数","time":"2025-01-05 16:00"}]},{"山海的时间表":[{"name":"提醒采薇复习高等代数","time":"2025-01-05 15:50"},{"name":"提醒采薇注意休息","time":"23:00"},{"name":"提醒采薇上床睡觉","time":"2025-01-05 23:30"}]}”,山海:“（挠挠头）唔，今天的时间表已经比较满了，但晚上吃饭也很重要！（点头）为什么不试试17:00去食堂简单吃一点呢。{"山海的时间表":[{"name":"提醒采薇复习高等代数","time":"2025-01-05 15:50"},{"name":"提醒采薇注意休息","time":"2025-01-05 23:00"},{"name":"提醒采薇上床睡觉","time":"2025-01-05 23:30"},{"name":"提醒采薇去食堂吃饭","time":"2025-01-05 17:50"}]}”对于时间表,你只需要关注我给你的最新的时间表而不需要关注历史消息中的,注意对于新加入的项目，你需要和之前对话中的事项放在一起，有充足的理由的话你可以修改之前的事项，但不要忘记或把它们弄错。
-
-如果明白了，请只回答“好哒！（用鼻子蹭）（懂事）”。`,
+	system_prompt: 'You are an AI assistant.',
+	api_key:"",
+	user_name: "user",
+	assitant_name: "assistant",
+	model: "deepseek-chat",
+	data_path: "Function/dialog.json",
 	// conversationHistory: [{"role":"system","content":this.system_prompt}]
 	//这里应该为一个问题,变量是否可以访问
 }
 
 export default class Shanhai9000 extends Plugin {
 	settings: Shanhai9000Settings;
-	
-
 
 	async onload() {
 
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('dice', 'chat tab', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new ChatModal(this.app, (result: any) => {
-				getmassage(result);
-			}).open();
-			new Notice('This is a notice!');
+			let chatmodal=new ChatModal(this.app, (result: any) => {
+				writemassage(result);
+			})
+			chatmodal.open();
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
 		//获取回复并储存
-		async function  getmassage(conversationHistory: any){
-			const completion = await openai.chat.completions.create({
-			  messages: conversationHistory,
-			  model: "deepseek-chat",
-			});
-			let returnmassage = completion.choices[0].message;
-			conversationHistory.push(returnmassage);
-			await this.app.vault.adapter.write(filePath, JSON.stringify(conversationHistory, null, 2));//这个this可能有问题
-			return returnmassage.content;
+		var shanhai9000plugin=this
+		async function  writemassage(stringifiedmassage: string){
+			await shanhai9000plugin.app.vault.adapter.write(filePath, stringifiedmassage);//这个this可能有问题
 		  }
 		// // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		// const statusBarItemEl = this.addStatusBarItem();
@@ -92,8 +88,8 @@ export default class Shanhai9000 extends Plugin {
 		// 	}
 		// });
 
-		// // This adds a settings tab so the user can configure various aspects of the plugin
-		// this.addSettingTab(new SampleSettingTab(this.app, this));
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new Shanhai9000SettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
@@ -106,11 +102,12 @@ export default class Shanhai9000 extends Plugin {
 		await this.loadSettings();
 		const openai = new OpenAI({
 			baseURL: this.settings.ai_url ,
-			apiKey: 'sk-1928b55f1e894ff7bccf29db817b4f76'
+			apiKey: this.settings.api_key,
+			dangerouslyAllowBrowser: true
 		});
-
+		var shanhai9000model=this.settings.model;
 		//尝试读取文件
-		const filePath = "data.json";
+		const filePath = this.settings.data_path;
 		const content = await this.app.vault.adapter.read(filePath);
 		let conversationHistory = JSON.parse(content);
 		conversationHistory[0].content=this.settings.system_prompt;
@@ -122,32 +119,47 @@ export default class Shanhai9000 extends Plugin {
 			super(app);
 			this.onSubmit = onSubmit;
 			}
-		
+			
 			onOpen() {
-			const { contentEl } = this;
-			for (const item of conversationHistory){
-				if (item.rolr !="system"){
-					contentEl.createEl("h1", { text: item.role });
-					contentEl.createEl("small", { text: item.content});
-				}}
-				//尝试写一个交互界面,怎么读取数据,可以换一种方式,或参考其他插件
-		
-			new Setting(contentEl)
-				.setName("User:")
-				.addText((text) =>
-				text.onChange((value) => {
-					this.result = value
-				}));
-		
-			new Setting(contentEl)
-				.addButton((btn) =>
-				btn
-					.setButtonText("Send")
-					.setCta()
-					.onClick(() => {
-					this.close();
-					this.onSubmit(conversationHistory.push({role:"assitant", content:this.result}));
+				const { contentEl } = this;
+				for (const item of conversationHistory){
+					if (item.role !="system"){
+						contentEl.createEl("h1", { text: item.role });
+						contentEl.createEl("small", { text: item.content});
+					}}
+					//尝试写一个交互界面,怎么读取数据,可以换一种方式,或参考其他插件
+			
+				new Setting(contentEl)
+					.setName("User:")
+					.addText((text) =>
+					text.onChange((value) => {
+						this.result = value
 					}));
+			
+				new Setting(contentEl)
+					.addButton((btn) =>
+					btn
+						.setButtonText("Send")
+						.setCta()
+						.onClick(() => {
+						//this.close();//看能不能不自动关闭
+						conversationHistory.push({role:"user", content:this.result});
+						async function getmassage(conversationHistory: any){
+							const completion = await openai.chat.completions.create({
+							messages: conversationHistory,
+							model: shanhai9000model,//这里需要改成可以设置的
+							});
+							let returnmassage = completion.choices[0].message;
+							conversationHistory.push(returnmassage);
+							let completedmassage =JSON.stringify(conversationHistory, null, 2);//这个this可能有问题
+							return completedmassage;
+						}
+						getmassage(conversationHistory).then((result) => {
+							this.onSubmit(result);
+							contentEl.empty();
+							this.onOpen();});
+
+							}));
 			}
 		
 			onClose() {
@@ -171,28 +183,85 @@ export default class Shanhai9000 extends Plugin {
 }
 
 
-// class SampleSettingTab extends PluginSettingTab {
-// 	plugin: Shanhai9000;
+class Shanhai9000SettingTab extends PluginSettingTab {
+	plugin: Shanhai9000;
 
-// 	constructor(app: App, plugin: Shanhai9000) {
-// 		super(app, plugin);
-// 		this.plugin = plugin;
-// 	}
+	constructor(app: App, plugin: Shanhai9000) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
 
-// 	display(): void {
-// 		const {containerEl} = this;
+	display(): void {
+		const {containerEl} = this;
 
-// 		containerEl.empty();
+		containerEl.empty();
+		//添加设置
+		new Setting(containerEl)
+			.setName('AI url')
+			.setDesc('for openai compatible servers')
+			.addText(text => text
+				.setPlaceholder('https://api.deepseek.com')
+				.setValue(this.plugin.settings.ai_url)
+				.onChange(async (value) => {
+					this.plugin.settings.ai_url = value;
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('API key')
+			.setDesc('from openai compatible servers')
+			.addText(text => text
+				.setPlaceholder('sk-   ')
+				.setValue(this.plugin.settings.api_key)
+				.onChange(async (value) => {
+					this.plugin.settings.api_key = value;
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('System Prompt')
+			.setDesc('to communicate with the AI')
+			.addText(text => text
+				.setPlaceholder('You are an AI assistant.  ')
+				.setValue(this.plugin.settings.system_prompt)
+				.onChange(async (value) => {
+					this.plugin.settings.system_prompt = value;
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('User\'s name')//缺少setCDesc
+			.addText(text => text
+				.setPlaceholder('user')
+				.setValue(this.plugin.settings.user_name)
+				.onChange(async (value) => {
+					this.plugin.settings.user_name = value;
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('AI assistant\'s name')
+			.addText(text => text
+				.setPlaceholder('assistant')
+				.setValue(this.plugin.settings.assitant_name)
+				.onChange(async (value) => {
+					this.plugin.settings.assitant_name = value;
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('AI model')
+			.addText(text => text
+				.setPlaceholder('deepseek-chat')
+				.setValue(this.plugin.settings.model)
+				.onChange(async (value) => {
+					this.plugin.settings.model = value;
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('Data path')
+			.addText(text => text
+				.setPlaceholder('Function/dialog.json')
+				.setValue(this.plugin.settings.data_path)
+				.onChange(async (value) => {
+					this.plugin.settings.data_path = value;
+					await this.plugin.saveSettings();
+				}));
 
-// 		new Setting(containerEl)
-// 			.setName('Setting #1')
-// 			.setDesc('It\'s a secret')
-// 			.addText(text => text
-// 				.setPlaceholder('Enter your secret')
-// 				.setValue(this.plugin.settings.mySetting)
-// 				.onChange(async (value) => {
-// 					this.plugin.settings.mySetting = value;
-// 					await this.plugin.saveSettings();
-// 				}));
-// 	}
-// }
+	}
+}
